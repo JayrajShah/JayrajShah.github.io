f11b317ac4c6b08c05935db2933bcf6323773816
Jayraj Shah <jayrajcshah@gmail.com>
2020-09-19 02:46:32 +0530

    Updates

---
+++ precache-manifest.3589744200347f25b485418d1a63cc97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a720b185c0abec19877489452366c8b5",
+    "url": "/JayrajShah/JayrajShah.github.io/index.html"
+  },
+  {
+    "revision": "7a54db218dde1f0b15a9",
+    "url": "/JayrajShah/JayrajShah.github.io/static/css/main.9d57234a.chunk.css"
+  },
+  {
+    "revision": "e7e7727d15705fd93a01",
+    "url": "/JayrajShah/JayrajShah.github.io/static/js/2.df9d5c0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JayrajShah/JayrajShah.github.io/static/js/2.df9d5c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a54db218dde1f0b15a9",
+    "url": "/JayrajShah/JayrajShah.github.io/static/js/main.8e8c59a9.chunk.js"
+  },
+  {
+    "revision": "9d5de91ebdcafd4e4606",
+    "url": "/JayrajShah/JayrajShah.github.io/static/js/runtime-main.2cd372e0.js"
+  },
+  {
+    "revision": "3b5635ac5d39c28200bf3993d90d09c7",
+    "url": "/JayrajShah/JayrajShah.github.io/static/media/Raleway-VariableFont_wght.3b5635ac.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JayrajShah/JayrajShah.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
